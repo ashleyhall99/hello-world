7028951df055f537472ff8e04cbcfefde44dabfd
Ashley Hall <59103743+ashleyhall99@users.noreply.github.com>
2023-09-04 16:15:41 -0400

    Revert "Update"
    This reverts commit 86be82b2af346673602db669fa70a88d7c10a123.

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,8 +5,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       System.out.println("Hello world!");
-       System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
+       System.out.println("Hello world!")
     }
     
 }
